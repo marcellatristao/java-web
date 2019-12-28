8902ff6cae2c8610c23c699a51f9b257f01058d5
MarcellaCanedo <marcellacanedo@hotmail.com>
2019-12-28 14:29:12 -0300

    UPDATE - Tipos primitivos de dados

--- first-program/src/cursojava/executavel/PrimeiraClasseJava.java
+++ first-program/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -2,13 +2,23 @@ package cursojava.executavel;
 
 public class PrimeiraClasseJava {
 	
+	/*Variável gobal é acessivel a todos e o seu valor é compartilhado*/
+	static int maiorIdadeGlobal = 30;
+	
 	/*Main é um método auto executável em Java*/
 	public static void main(String[] args) {
 		
-		/*Escrevendo textos no console*/
-		System.out.println("First program in Java - Formation JavaWeb");
+		/*Variavel local pois pertence somente a este método e o valor fica dentro do método*/
+		int maiorIdade = 18;
+		System.out.println("Valor da variável local = " + maiorIdade);
+		System.out.println("Valor da variável global = " + maiorIdadeGlobal);
+		metodo2();
+	}
+	
+	public static void metodo2() {
+		int mediaAno =50;
+		System.out.println("Valor da variável global = " + maiorIdadeGlobal);
 		
-		System.out.println("XD");
 	}
 	
 }
