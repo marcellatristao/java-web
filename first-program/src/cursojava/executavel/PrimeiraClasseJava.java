345c6ce4d46c27f1fbd142bd4ed55b5820924497
MarcellaCanedo <marcellacanedo@hotmail.com>
2019-12-28 09:47:50 -0300

    UPDATE - Initial commit java-web

---
+++ first-program/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,14 @@
+package cursojava.executavel;
+
+public class PrimeiraClasseJava {
+	
+	/*Main é um método auto executável em Java*/
+	public static void main(String[] args) {
+		
+		/*Escrevendo textos no console*/
+		System.out.println("First program in Java - Formation JavaWeb");
+		
+		System.out.println("XD");
+	}
+	
+}
