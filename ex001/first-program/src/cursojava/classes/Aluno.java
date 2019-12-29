5159031823ba9c36df70f317eeb8ec441dd85a31
MarcellaCanedo <marcellacanedo@hotmail.com>
2019-12-29 13:09:37 -0300

    UPDATE - Media alunos

--- ex001/first-program/src/cursojava/classes/Aluno.java
+++ ex001/first-program/src/cursojava/classes/Aluno.java
@@ -13,6 +13,11 @@ public class Aluno {
 	private String dataMatricula;
 	private String nomeEscola;
 	private String serieMatriculada;
+	private double nota1;
+	private double nota2;
+	private double nota3;
+	private double nota4;
+	private boolean aprovado;
 	
 	//Sempre que o new é utilizado o comando abaixo é criado vazio por padrão
 	
@@ -115,6 +120,60 @@ public class Aluno {
 	public void setSerieMatriculada(String serieMatriculada) {
 		this.serieMatriculada = serieMatriculada;
 	}
+
+	public double getNota1() {
+		return nota1;
+	}
+
+	public void setNota1(double nota1) {
+		this.nota1 = nota1;
+	}
+
+	public double getNota2() {
+		return nota2;
+	}
+
+	public void setNota2(double nota2) {
+		this.nota2 = nota2;
+	}
+
+	public double getNota3() {
+		return nota3;
+	}
+
+	public void setNota3(double nota3) {
+		this.nota3 = nota3;
+	}
+
+	public double getNota4() {
+		return nota4;
+	}
+
+	public void setNota4(double nota4) {
+		this.nota4 = nota4;
+	}
 	
+	public void setAprovado(boolean aprovado) {
+		this.aprovado = aprovado;
+	}
+	
+	
+	public boolean getaprovado() {
+		return aprovado;
+	}
+
+	/*Método que retorna a média do aluno*/
+	public double getMediaNota() {
+		return(nota1 + nota2 + nota3 + nota4) / 4;
+	}
+	
+	public boolean getAprovado() {
+		if(getMediaNota() >= 70) {
+			System.out.println(aprovado = true);
+		}else {
+			System.out.println(aprovado = false);
+		}
+		return aprovado;
+	}
 	
 }
--- ex001/first-program/src/cursojava/executavel/PrimeiraClasseJava.java
+++ ex001/first-program/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -9,10 +9,10 @@ public class PrimeiraClasseJava {
 		// new Aluno() é uma instancia/ criação de um objeto!!
 		
 		//aluno1 é uma referência para o aluno
-		
-		Aluno aluno1 = new Aluno(); /*João*/
-		Aluno aluno2 = new Aluno(); /*Pedro*/
-		
+	
+		Aluno aluno1 = new Aluno(); /*Objeto aluno 1*/
+		Aluno aluno2 = new Aluno(); /*Objeto aluno 2*/
+	
 		aluno1.setNome("Marcella Canedo Tristão");
 		aluno1.setIdade(19);
 		aluno1.setDataNascimento("02/05/2000");
@@ -23,30 +23,40 @@ public class PrimeiraClasseJava {
 		aluno1.setDataMatricula("29/12/2019");
 		aluno1.setNomeEscola("Auxiliadora");
 		aluno1.setSerieMatriculada("5ª");
+		aluno1.setNota1(44);
+		aluno1.setNota2(73);
+		aluno1.setNota3(93.9);
+		aluno1.setNota4(56);
 		
 		System.out.println("NOME COMPLETO: " +aluno1.getNome() + "\nIDADE: " +aluno1.getIdade() + 
 				"\nDATA NASCIMENTO: " +aluno1.getDataNascimento() + "\nREGISTRO GERAL: " +aluno1.getRegistroGeral() 
 				+ "\nNUMERO CPF: " +aluno1.getNumeroCpf() + "\nNOME MAE: " +aluno1.getNomeMae() + "\nNOME PAI: " +aluno1.getNomePai() +
 				"\nDATA MATRICULA: " +aluno1.getDataMatricula() + "\nNOME ESCOLA" + "\nSERIE MATRICULADA: " +aluno1.getSerieMatriculada());	
 		
+		System.out.println("\nMEDIA DA NOTA: " +aluno1.getMediaNota()); //CHAMADA DE MÉTODO
+		
+		if(aluno1.getaprovado() == false) {
+			System.out.println("Aluno reprovado!");	
+		}else {
+			System.out.println("Aluno aprovado!");
+		}
 		
 		System.out.println("\n\n------------------------------\n");
 		
-		aluno2.setNome("Pedro da Silva");
-		aluno2.setIdade(16);
-		aluno2.setDataNascimento("05/02/2000");
-		aluno2.setRegistroGeral("0000000");
-		aluno2.setNumeroCpf("000-000-000-00");
-		aluno2.setNomeMae("Maria");
-		aluno2.setNomePai("João");
-		aluno2.setDataMatricula("10/02/2019");
-		aluno2.setNomeEscola("Auxiliadora");
-		aluno2.setSerieMatriculada("6ª");	
-		
-		System.out.println("NOME COMPLETO: " +aluno2.getNome() + "\nIDADE: " +aluno2.getIdade() + 
-				"\nDATA NASCIMENTO: " +aluno2.getDataNascimento() + "\nREGISTRO GERAL: " +aluno2.getRegistroGeral() 
-				+ "\nNUMERO CPF: " +aluno2.getNumeroCpf() + "\nNOME MAE: " +aluno2.getNomeMae() + "\nNOME PAI: " +aluno2.getNomePai() +
-				"\nDATA MATRICULA: " +aluno2.getDataMatricula() + "\nNOME ESCOLA" + "\nSERIE MATRICULADA: " +aluno2.getSerieMatriculada());	
+		/*
+		 * aluno2.setNome("Pedro da Silva"); aluno2.setIdade(16);
+		 * aluno2.setDataNascimento("05/02/2000"); aluno2.setRegistroGeral("0000000");
+		 * aluno2.setNumeroCpf("000-000-000-00"); aluno2.setNomeMae("Maria");
+		 * aluno2.setNomePai("João"); aluno2.setDataMatricula("10/02/2019");
+		 * aluno2.setNomeEscola("Auxiliadora"); aluno2.setSerieMatriculada("6ª");
+		 * 
+		 * System.out.println("NOME COMPLETO: " +aluno2.getNome() + "\nIDADE: "
+		 * +aluno2.getIdade() + "\nDATA NASCIMENTO: " +aluno2.getDataNascimento() +
+		 * "\nREGISTRO GERAL: " +aluno2.getRegistroGeral() + "\nNUMERO CPF: "
+		 * +aluno2.getNumeroCpf() + "\nNOME MAE: " +aluno2.getNomeMae() + "\nNOME PAI: "
+		 * +aluno2.getNomePai() + "\nDATA MATRICULA: " +aluno2.getDataMatricula() +
+		 * "\nNOME ESCOLA" + "\nSERIE MATRICULADA: " +aluno2.getSerieMatriculada());
+		 */	
 		
 	}
 }
